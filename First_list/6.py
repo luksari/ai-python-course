cd10e12adb5baca70f29d2e2f9d80923b352211e
luksari <luksari23@gmail.com>
2018-03-05 20:22:45 +0100

    Ended first list, fixing bugs

--- First_list/6.py
+++ First_list/6.py
@@ -22,11 +22,6 @@ for probe in irisData:
 
 data = np.column_stack((_class, sep_len, sep_width, pet_len, pet_width))
 
-pet_len = []
-sep_len = []
-pet_width = []
-sep_width = []
-
 avg_pet_len = []
 avg_pet_width = []
 avg_sep_len = []
@@ -34,6 +29,12 @@ avg_sep_width = []
 
 
 def showData(key):
+
+    pet_len = []
+    sep_len = []
+    pet_width = []
+    sep_width = []
+
     index = 0
     for val in data:
         if val[0] == key:
@@ -54,10 +55,10 @@ def showData(key):
     elif key == "Iris-virginica":
         index = 2
     print("\n" + key + " average attributes values:"
-      + "\npetal length: " + str(avg_pet_len[index])[:5]
-        + "\npetal width: " + str(avg_pet_width[index])[:5]
-        + "\nsepal length: " + str(avg_sep_len[index])[:5]
-        + "\nsepal width: " + str(avg_sep_width[index])[:5])
+          + "\npetal length: " + str(avg_pet_len[index])[:5]
+          + "\npetal width: " + str(avg_pet_width[index])[:5]
+          + "\nsepal length: " + str(avg_sep_len[index])[:5]
+          + "\nsepal width: " + str(avg_sep_width[index])[:5])
 
 
 showData("Iris-setosa")
