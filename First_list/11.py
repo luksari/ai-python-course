b59124c2ab76137af49dfe4c6734fffb8984bead
luksari <luksari23@gmail.com>
2018-03-07 19:28:41 +0100

    Updated classifier function

--- First_list/11.py
+++ First_list/11.py
@@ -1,72 +1,35 @@
 import numpy as np
-from sklearn import preprocessing
-import matplotlib.pyplot as plt
 
-N = 5
-
+N = 10
 
 def classifier(x):
-    if x[0] >= 0 and x[1] >= 0:
-        return 1
-    elif x[0] < 0 and x[1] > 0:
+    if g1(x) > g2(x):
         return 2
-    elif x[0] <= 0 and x[1] <= 0:
-        return 3
-    elif x[0] > 0 and x[1] <0:
-        return 4
+    else:
+        return 1
+
+def g1(x):
+    return -x[0] + x[1]
 
 
-first_attr = np.random.normal(2, 1, 2*N)
-second_attr = 10*np.random.rand(2*N)
-
-# first_attr = np.random.rand(N, 2)
-# second_attr = np.random.rand(N, 2)-1
-
-data = np.vstack((first_attr, second_attr))
-data = data.conj().transpose()
-
-min_x, min_y, max_x, max_y, offset = min(data[:, 0]), min(
-    data[:, 1]), max(data[:, 0]), max(data[:, 1]), 1
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-plt.scatter(data[:,0],data[:,1])
-
-left, right = ax.get_xlim()
-low, high = ax.get_ylim()
+def g2(x):
+    return x[0] - x[1]
 
 
-plt.plot([-5, 10], [0, 0], "black")
-plt.plot([0, 0], [-5, 10], "black")
+class1 = np.random.rand(N, 2)
+class2 = np.random.rand(N, 2)-1
 
-plt.grid();
+data = np.vstack((class1, class2))
 
-plt.show()
+y1 = [g1(data[i, :]) for i in range(2*N)]
+y2 = [g2(data[i, :]) for i in range(2*N)]
 
-labels=np.array([classifier(data[i,:]) for i in range(2*N)])
+y1 = np.round(y1, 2)
+y2 = np.round(y2, 2)
+print('Wartości funkcji klasyfikujacych')
+print(y1)
+print(y2)
 
-c1 = (labels==1).nonzero()
-c2 = (labels==2).nonzero()
-c3 = (labels==3).nonzero()
-c4 = (labels==4).nonzero()
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-
-plt.scatter(data[:, 0], data[:, 1])
-
-left, right = ax.get_xlim()
-low, high = ax.get_ylim()
-
-plt.plot([-5, 10], [0, 0], "black")
-plt.plot([0, 0], [-5, 10], "black")
-
-plt.grid()
-
-plt.scatter(data[c1,0],data[c1,1],c='b')
-plt.scatter(data[c2,0],data[c2,1],c='r')
-plt.scatter(data[c3,0],data[c3,1],c='g')
-plt.scatter(data[c4,0],data[c4,1],c='y')
-
-plt.show()
+labels = np.array([classifier(data[i, :]) for i in range(2*N)])
+print('Decyzje klasyfikatora:')
+print(labels)
--- First_list/12.py
+++ First_list/12.py
@@ -1,43 +1,39 @@
 import numpy as np
-from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
 N = 10
 
 
 def classifier(x):
-    if x[0] >= 0 and x[1] >= 0:
-        return 1
-    elif x[0] < 0 and x[1] > 0:
+    if g1(x) > g2(x):
         return 2
-    elif x[0] <= 0 and x[1] <= 0:
-        return 3
-    elif x[0] > 0 and x[1] < 0:
-        return 4
+    else:
+        return 1
 
 
-class1 = np.random.normal(1, 8, 2*N)
-class2 = 10*np.random.rand(1,2*N) -7
+def g1(x):
+    return -x[0] + x[1]
 
+
+def g2(x):
+    return x[0] - x[1]
+
+
+f1 = np.random.rand(N) - 2
+s1 = np.random.rand(N) + 3
+
+
+f2 = np.random.rand(N) + 3
+s2 = np.random.rand(N) - 3
+
+class1 = np.column_stack((f1, s1))
+class2 = np.column_stack((f2, s2))
 data = np.vstack((class1, class2))
-data = data.conj().transpose()
-
-
-
-min_x, min_y, max_x, max_y, offset = min(data[:, 0]), min(
-    data[:, 1]), max(data[:, 0]), max(data[:, 1]), 1
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
 plt.scatter(data[:, 0], data[:, 1])
-
-left, right = ax.get_xlim()
-low, high = ax.get_ylim()
-
-
-plt.plot([-20, 20], [0, 0], "black")
-plt.plot([0, 0], [-20, 20], "black")
 
 plt.grid()
 
@@ -45,26 +41,26 @@ plt.show()
 
 labels = np.array([classifier(data[i, :]) for i in range(2*N)])
 
+
 c1 = (labels == 1).nonzero()
 c2 = (labels == 2).nonzero()
-c3 = (labels == 3).nonzero()
-c4 = (labels == 4).nonzero()
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
 plt.scatter(data[:, 0], data[:, 1])
 
-left, right = ax.get_xlim()
-low, high = ax.get_ylim()
+y1 = [g1(data[i, :]) for i in range(2*N)]
+y2 = [g2(data[i, :]) for i in range(2*N)]
 
-plt.plot([-20, 20], [0, 0], "black")
-plt.plot([0, 0], [-20, 20], "black")
+y1 = np.round(y1, 2)
+y2 = np.round(y2, 2)
+
+plt.plot(y1, -y2)
+
 plt.grid()
 
 plt.scatter(data[c1, 0], data[c1, 1], c='b')
 plt.scatter(data[c2, 0], data[c2, 1], c='r')
-plt.scatter(data[c3, 0], data[c3, 1], c='g')
-plt.scatter(data[c4, 0], data[c4, 1], c='y')
 
 plt.show()
